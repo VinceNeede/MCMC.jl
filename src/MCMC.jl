3d260a8aeb95a6c9337025721826cf2a18a25e1f
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-04-04 22:11:22 +0200

    added PI MCMC module includes

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -3,6 +3,8 @@ module MCMC
 include("imports.jl")
 include("mcmc.jl")
 include("abstract_mcmc.jl")
+include("pi_mcmc/abstract_pimcmc.jl")
+include("pi_mcmc/pimcmc.jl")
 include("exports.jl")
 
 end # module MCMC
