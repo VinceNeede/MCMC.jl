7e9f77931c39b6bc6eb7a559a08ddb25de425067
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-04-05 09:57:34 +0200

    checkdocs was inserted in the wrong function

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,17 @@
 using Documenter, MCMC, HDF5
 
 makedocs(
-	sitename="MCMC", 
-	modules=[MCMC],
-	pages = [
-		"Home" => "index.md",
-		"Documentation" => [
-			"Abstract MCMC" => "abstract_mcmc.md",
-			"Utilities" => "utils.md",
-			"PiMCMC" => "pi_mcmc.md",
-		]
-	],
-	)
+    sitename="MCMC",
+    modules=[MCMC],
+    pages=[
+        "Home" => "index.md",
+        "Documentation" => [
+            "Abstract MCMC" => "abstract_mcmc.md",
+            "Utilities" => "utils.md",
+            "PiMCMC" => "pi_mcmc.md",
+        ]
+    ],
+    checkdocs=:export,
+)
 
-	deploydocs(; repo="github.com/VinceNeede/MCMC.jl", devbranch="main", push_preview=true, checkdocs=:export)
+deploydocs(; repo="github.com/VinceNeede/MCMC.jl", devbranch="main", push_preview=true)
