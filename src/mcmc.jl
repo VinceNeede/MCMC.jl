f99e907314e66d08005f306f62b31cbfa5290c30
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-04-07 17:31:47 +0200

    fixed error in filtering only module logs and reordered Early and Acrive filters

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -13,19 +13,21 @@ utility function to create a temporary file for saving the checkpoint of the MCM
 default_checkpoint_file() = (filename = tempname() * ".h5"; @info "saving checkpoint in temp file" filename; filename)
 
 """
-	mcmc_logger(every::Int=100)
+	mcmc_logger(every::Int=100; all::Bool=false)
 
 utility function to create a temporary file for saving the logs of the MCMC run.
-The logs are printed every `every` iterations.
+The logs are printed every `every` iterations. With `all==false` only the final 
+log with the conclusion of the run is printed.
 """
-function mcmc_logger(every::Int=100)
-	filename = tempname() * ".log"
-	@info "saving log in temp file" filename
-	function filter(log)
-		if log._module == MCMC && log._id == :MCMC_run_iteration
-			return true
-		end
-		return false
-	end
-	return ActiveFilteredLogger(log -> log.iteration % every == 0, EarlyFilteredLogger(filter, FileLogger(filename)))
+function mcmc_logger(every::Int=100; all::Bool=false)
+    filename = tempname() * ".log"
+    @info "saving log in temp file" filename
+    function filter(log)
+        log._module == MCMC || return true
+        if all || log.id == :MCMC_run_iteration
+            return true
+        end
+        return false
+    end
+    return EarlyFilteredLogger(filter, ActiveFilteredLogger(log -> log.kwargs[:iteration] % every == 0, FileLogger(filename)))
 end
