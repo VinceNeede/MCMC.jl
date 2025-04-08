43b7384d15f4b00addcbf04dc13029435d63d9ee
VinceNeede <v.bisogno3@studenti.unipi.it>
2025-04-08 23:44:03 +0200

    moved pimcmc.jl to src/

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -4,7 +4,7 @@ include("imports.jl")
 include("mcmc.jl")
 include("abstract_mcmc.jl")
 include("base_mcmc.jl")
-include("pi_mcmc/pimcmc.jl")
+include("pimcmc.jl")
 include("exports.jl")
 
 end # module MCMC
